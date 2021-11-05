78e628831ac218a1f0db55dbfc4eeb7983ef4c74
Mustan Pathetic <patheticmustan@gmail.com>
2021-11-05 22:14:30 +0000

    top level async

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-
 const { LASTFM_API_KEY, GH_PERSONAL_TOKEN } = process.env;
 
-console.log(typeof LASTFM_API_KEY);
-
-const data = await fetch(
-    "http://ws.audioscrobbler.com/2.0/?" +
-    "method=user.getrecenttracks&user=patheticmustan&format=json&" +
+// all i wanted was top level async
+(async () => {
+    const data = await fetch(
+        "http://ws.audioscrobbler.com/2.0/?" +
+        "method=user.getrecenttracks&user=patheticmustan&format=json&" +
     `api_key=${process.env.LASTFM_API_KEY}`
-);
-const json = (await data.json()).recenttracks;
-
-const songList = json.track.map(v => `${v.name} - ${v.artist["#text"]}`);
-
-console.log(songList.join("\n"));
+    );
+    const json = (await data.json()).recenttracks;
+    
+    const songList = json.track.map(v => `${v.name} - ${v.artist["#text"]}`);
+    
+    console.log(songList.join("\n"));
+})()
