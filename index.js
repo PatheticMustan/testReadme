efedce9d70412f7ceb306dd71422e5417d3de803
Mustan Pathetic <patheticmustan@gmail.com>
2021-11-05 22:12:56 +0000

    stupid paren

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const { LASTFM_API_KEY, GH_PERSONAL_TOKEN } = process.env;
 
 console.log(typeof LASTFM_API_KEY);
 
-const tracks =
-    (await
-        (await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=patheticmustan&api_key=${process.env.LASTFM_API_KEY}&format=json`)
-    ).json()).recenttracks;
+const data = await fetch(
+    "http://ws.audioscrobbler.com/2.0/?" +
+    "method=user.getrecenttracks&user=patheticmustan&format=json&" +
+    `api_key=${process.env.LASTFM_API_KEY}`
+);
+const json = (await data.json()).recenttracks;
 
-const songList = tracks.track.map(v => `${v.name} - ${v.artist["#text"]}`);
+const songList = json.track.map(v => `${v.name} - ${v.artist["#text"]}`);
 
 console.log(songList.join("\n"));
